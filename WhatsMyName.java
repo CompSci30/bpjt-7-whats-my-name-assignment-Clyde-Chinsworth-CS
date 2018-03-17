cf5013485effc095c4ae82a3b005ce7e5f26ad6f
Alex Yang <a.yang2@share.epsb.ca>
2018-03-17 17:20:43 +0000

    Added first name input

--- WhatsMyName.java
+++ WhatsMyName.java
@@ -7,6 +7,7 @@ public class WhatsMyName
 	public static void main(String args[])
 	{
 		Scanner kbReader = new Scanner(System.in);
-		
+		System.out.print("What is your first name?");
+		String name1 = kbReader.next(); 
 	}
 }
